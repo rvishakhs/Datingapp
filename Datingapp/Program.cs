a13ce983acdf5fd456ad2bb7ee2b1ff6ae84cddb
rvishakhs <rvishakhs@gmail.com>
2024-03-16 23:18:38 +0000

    Second Commit Entity Framework and Migration done

--- Datingapp/Program.cs
+++ Datingapp/Program.cs
@@ -1,7 +1,13 @@
+using Datingapp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
