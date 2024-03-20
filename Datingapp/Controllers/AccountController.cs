2b426a49559277eaf498ff05772933cf4857053f
rvishakhs <rvishakhs@gmail.com>
2024-03-20 10:54:28 +0000

    login details added

--- Datingapp/Controllers/AccountController.cs
+++ Datingapp/Controllers/AccountController.cs
@@ -41,6 +41,25 @@ public class AccountController : BasicApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.Username.ToLower());
+        
+        if (user == null ) return Unauthorized("Invalid username");
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+        }
+
+        return user; 
+    }
+
     private async Task<bool> UserExists(string username)
     {
        return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
