36f02b9c3831498312ff68f31c23ca260ff3fa47
rvishakhs <rvishakhs@gmail.com>
2024-04-16 10:38:48 +0100

    dropdown box not working need to fix

--- mingle/src/app/nav/nav.component.ts
+++ mingle/src/app/nav/nav.component.ts
@@ -1,39 +1,39 @@
-import { Component, NgModule } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { AccountService } from '../_services/account.service';
-
+import { CommonModule } from '@angular/common';
+import { BsDropdownConfig } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule,],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
+  providers: [{ provide: BsDropdownConfig, useValue: { isAnimated: true, autoClose: true } }]
 })
 export class NavComponent {
 
   model: any = {};
-
+  loggedIn: boolean = false;
   constructor(private http : HttpClient) {
 
   }
-  // login() {
-  //   this.accountService.login(this.model).subscribe(response => {
-  //     console.log(response);
-  //   }, error => {
-  //     console.log(error);
-  //   }
-  //   )
-  // }
 
   login() {
     this.http.post('http://localhost:5284/api/Account/login',this.model).subscribe(response => {
           console.log(response);
+          this.loggedIn = true;
          }, error => {
            console.log(error);
          }
          )
   }
+
+  loggedOut() {
+    this.loggedIn =false;
+  }
+
+
 
 }
